59d37180a0b3c42d99e7dfb1427c071c1d7a6a56
Eros Cedeño <eycer1@gmail.com>
2023-05-21 12:48:00 -0400

    login page

---
+++ lib/pages/login.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  static String tag = 'login';
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    final logo = Hero(
+        tag: 'hero',
+        child: CircleAvatar(
+            backgroundColor: Colors.transparent,
+            radius: 48.0,
+            child: Image.asset('assets/logo.png')));
+
+    final email = TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      autofocus: false,
+      decoration: InputDecoration(
+          hintText: 'Email',
+          contentPadding: const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+    );
+
+    final password = TextFormField(
+      autofocus: false,
+      obscureText: true,
+      decoration: InputDecoration(
+          hintText: 'Password',
+          contentPadding: const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+    );
+
+    final loginButton = Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16.0),
+      child: Material(
+          borderRadius: BorderRadius.circular(30.0),
+          shadowColor: Colors.lightBlueAccent.shade100,
+          color: Colors.lightBlueAccent,
+          elevation: 5.0,
+          child: MaterialButton(
+            height: 42.0,
+            onPressed: () {},
+            // color: Colors.lightBlueAccent,
+            child: const Text(
+              'Log In',
+              style: TextStyle(color: Colors.white),
+            ),
+          )),
+    );
+
+    final forgotLabel = TextButton(
+      onPressed: () {},
+      child: const Text('Forgot Password?',
+          style: TextStyle(color: Colors.black54)),
+    );
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: ListView(
+          shrinkWrap: true,
+          padding: const EdgeInsets.only(left: 24.0, right: 24.0),
+          children: <Widget>[
+            logo,
+            const SizedBox(height: 48.0),
+            email,
+            const SizedBox(height: 8.0),
+            password,
+            const SizedBox(height: 24.0),
+            loginButton,
+            forgotLabel
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/routes/routes.dart
@@ -0,0 +1,7 @@
+import 'package:pine/pages/home.dart';
+import 'package:pine/pages/login.dart';
+
+final routes = {
+  '/': (context) => const HomePage(),
+  '/login': (context) => const LoginPage()
+};
